d810141846cd89f4ba94a56ce65c248020379469
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-06 23:49:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,33 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '$';
 
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("Stars. || Broadcast", 'https://twitch.tv//9ivv')
+    console.log('')
+    console.log('')
+    console.log('╔[════════════════════════════════════════════════════════════════]╗')
+    console.log(`[Start] ${new Date()}`);
+    console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════════════════════════════]╗');
+    console.log(`Logged i as * [ " ${client.user.username} " ]`);
+    console.log('')
+    console.log('Informatins :')
+    console.log('')
+    console.log(`servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Users! [ " ${client.users.size} " ]`);
+    console.log(`channels! [ " ${client.channels.size} " ]`);
+    console.log('╚[════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════]╗')
+    console.log(' Bot Is Online')
+    console.log('╚[════════════]╝')
+    console.log('')
+    console.log('')
+  });
+
 client.on('message', message => {
     if(!message.member.hasPermissions('ADMINISTRATOR')) return;
     if(message.content.startsWith(prefix + 'bc')) {
